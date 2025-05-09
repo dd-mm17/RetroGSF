8420850d6c55696de71324f0b6ee82b6eb781129
dd-mm17 <diego.meraldi@epfl.ch>
2025-05-09 22:40:30 +0200

    implementing and fixing tests

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,96 +1,31 @@
-"""Sphinx configuration."""
+# Configuration file for the Sphinx documentation builder.
 
-# This file is execfile()d with the current directory set to its containing dir.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-#
-# All configuration values have a default; values that are commented out
-# serve to show the default.
+project = 'RetroGSF'
+copyright = '2024'
+author = 'Diego Meraldi, Witek Huguenin-Dezot, Lea Lombard'
 
-import os
-import shutil
-import sys
-from importlib.metadata import metadata
+# The full version, including alpha/beta/rc tags
+release = '0.0.1'
 
-# -- Path setup 
-
-__location__ = os.path.dirname(__file__)
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.join(__location__, "../../src"))
-
-# -- Run sphinx-apidoc 
-# This hack is necessary since RTD does not issue `sphinx-apidoc` before running
-# `sphinx-build -b html . _build/html`. See Issue:
-# https://github.com/readthedocs/readthedocs.org/issues/1139
-# DON'T FORGET: Check the box "Install your project inside a virtualenv
-# Additionally it helps us to avoid running apidoc manually
-
-try:  # for Sphinx >= 1.7
-    from sphinx.ext import apidoc
-except ImportError:
-    from sphinx import apidoc
-
-output_dir = os.path.join(__location__, "api")
-module_dir = os.path.join(__location__, "../../src/retrogsf")
-try:
-    shutil.rmtree(output_dir)
-except FileNotFoundError:
-    pass
-
-try:
-    import sphinx
-
-    cmd_line = f"sphinx-apidoc --implicit-namespaces -f -o {output_dir} {module_dir}"
-
-    args = cmd_line.split(" ")
-    if tuple(sphinx.__version__.split(".")) >= ("1", "7"):
-        # This is a rudimentary parse_version to avoid external dependencies
-        args = args[1:]
-
-    apidoc.main(args)
-except Exception as e:
-    print(f"Running `sphinx-apidoc` failed!\n{e}")
-
-# -- Project information
-
-_metadata = metadata("retrogsf")
-
-project = _metadata["Name"]
-author = _metadata["Author-email"].split("<", 1)[0].strip()
-copyright = f"2024, {author}"
-
-version = _metadata["Version"]
-release = ".".join(version.split(".")[:2])
-
-
-# -- General configuration
-
+# Add any Sphinx extension module names here, as strings
 extensions = [
-    "myst_parser",
-    "sphinx_copybutton",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.viewcode",
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'myst_parser',
 ]
 
-templates_path = ["_templates"]
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
 
-exclude_patterns = [
-    "Thumbs.db",
-    ".DS_Store",
-    ".ipynb_checkpoints",
-]
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+exclude_patterns = []
 
-suppress_warnings = [
-    'myst.header'
-]
+# The theme to use for HTML and HTML Help pages.
+html_theme = 'furo'
 
-# -- Options for HTML output
-
-html_theme = "furo"
-html_static_path = ["_static"]
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
